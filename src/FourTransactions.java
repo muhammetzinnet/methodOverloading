5ad36adbd202b61cb83b3d6f931df33042200efa
https://github.com/muhammetzinnet/Veriable <maicek@hotmail.com>
2022-09-25 00:32:56 +0300

    Initial commit

---
+++ src/FourTransactions.java
@@ -0,0 +1,9 @@
+public class FourTransactions {
+    public int Gather(int number_1, int number_2){
+        return  number_1 + number_2;
+    }
+
+    public int Gather(int number_1, int number_2, int number_3){
+        return number_1 + number_2;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FourTransactions fourTransactions = new FourTransactions();
+        System.out.println(fourTransactions.Gather(3,9));
+        System.out.println(fourTransactions.Gather(6,3,9));
+    }
+}
